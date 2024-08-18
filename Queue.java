bf67f9812397aa876295b1175421013c9eb6e33e
TeetatB <teetatmix36108@gmail.com>
2024-08-18 21:27:08 +0700

    adding comment for hw4

--- Queue.java
+++ Queue.java
@@ -3,35 +3,39 @@ public class Queue {
   OrderNode head;
   OrderNode tail;
   
-  public void Push(OrderNode node){
+  public void Push(OrderNode node){ // Pushing a node that contain orders into queue
     //START FIX HERE
-        if (head == null && tail == null){
+        if (head == null && tail == null){ // check if node is empty
+        //when there is no node in Linked list change head and tail address to node address
             head = node;
             tail = node;
         }else{
+          //when node is not empty push it to the back of Linked list and change it to tail
             tail.next = node;
             tail = tail.next;
         }
     //END FIX HERE
   }
   
-  public void Pop(){
+  public void Pop(){ // pop out the first node of Linked list
     //START FIX HERE
-        if (head != null && tail != null){
-            if (head != tail){
+        if (head != null && tail != null){ //check if Linked list is empty
+            if (head != tail){ //check if there is only 1 node in LinkedList
+              //when there are more than 1 node in Linked List change head address to its next node
                 OrderNode curr = head.next;
                 head = curr;
             }else{
+              //if there is only 1 node in Linked List change head and tail address to null
                 head = null;
                 tail = null;
             }
         } else {
-            System.out.println("Error: Queue Underflow");
+            System.out.println("Error: Queue Underflow"); //If Linked List is empty, then print Error:Queue Undeflow
         }
     //END FIX HERE
   }
   
-  public OrderNode Top(){
+  public OrderNode Top(){ //get the head of Linked List 
     //START FIX HERE
       return head;
     //END FIX HERE
--- Stack.java
+++ Stack.java
@@ -2,37 +2,37 @@ public class Stack {
   // Implement Stack using Linked List without tail
   Node head;
   
-  public void Push(Node node){
+  public void Push(Node node){ //function which adds node to the front of Linked List 
     //START FIX HERE
-        if (head == null){
-            // Do something
+        if (head == null){ //check if Linked list is empty
+            //When list is empty change head address to node
             head = node;
         }else{
-            // Do something else
-            node.next = head;
-            head = node;
+            //When list is not empty 
+            node.next = head; //change node next node address to head
+            head = node; //change head address to node address
         }
     // END FIX HERE
   }
   
-  public void Pop(){
+  public void Pop(){ //function which delete node at the front of Linked list (Last out)
     //START FIX HERE
-        if (!IsEmpty()){
-            // Do something
+        if (!IsEmpty()){ //check if Linked list is empty
+            //if true then change head address to its next node address;
             head = head.next;
-        }else{
+        }else{ //if Linked List is not empty print Error: Stack Underflow
             System.out.println("Error: Stack Underflow");
         }
     // END FIX HERE
   }
   
-  public Node Top(){
+  public Node Top(){ //function which return Linked List first node (view first node)
     //START FIX HERE
         return head;
     // END FIX HERE
   }
 
-  public Boolean IsEmpty() {
+  public Boolean IsEmpty() { //function which check if Linked List is empty
     //START FIX HERE
         return head == null;
     // END FIX HERE
