c1366005177e774dfd32c420d5d3206020e4765e
RAFFAN <134605328+raffanids@users.noreply.github.com>
2024-08-19 21:24:52 +0700

    Create Q5.java

---
+++ hw4/Q5.java
@@ -0,0 +1,51 @@
+/*จงสร้างคลาสที่ชื่อว่า Stack ที่สามารถบรรจุ Node ไว้ตามหลักการ LIFO ตามที่โจทย์กำหนดให้สมบูรณ์
+
+ข้อนี้โจทย์บังคับให้คุณสร้าง Singly Linked List without tail เพื่อเป็น Stack
+
+เมื่อสมบูรณ์แล้ว ให้ Copy โค้ดเติมในช่องข้างล่างให้เหมาะสม
+
+ตรวจสอบผลลัพธ์ให้ดี ว่าโค้ดคุณรันผ่าน Testcase ทุกอัน (รวมทั้งที่อาจารย์ซ่อนไว้ด้วย) ว่าผ่านทุกอันรึเปล่า
+
+และเมื่อ Submit โค้ดในหน้านี้เสร็จแล้วอย่าลืมส่งรายชื่อเพื่อนในกลุ่มใน Link Assignment (สีแดง) ด้วย (สำคัญมาก)
+
+* ให้คอมเม้นต์อธิบายการทำงานของฟังก์ชันที่สำคัญ หากการคอมเม้นต์มีน้อยเกินไปคะแนนของคุณจะถูกหัก 50 เปอร์เซ็นของคะแนนที่ได้*/
+
+public class Stack {
+  // Implement Stack using Linked List without tail
+  Node head;
+  
+  public void Push(Node node){
+    //START FIX HERE
+        if (head == null){
+            // Do something
+        }else{
+            // Do something else
+        }
+    // END FIX HERE
+  }
+  
+  public void Pop(){
+    //START FIX HERE
+        if (true){
+            // Do something
+        }else{
+            System.out.println("Error: Stack Underflow");
+        }
+    // END FIX HERE
+  }
+  
+  public Node Top(){
+    //START FIX HERE
+        return null;
+    // END FIX HERE
+  }
+
+  public Boolean IsEmpty() {
+    //START FIX HERE
+        return true;
+    // END FIX HERE
+  }
+
+  
+  
+}
