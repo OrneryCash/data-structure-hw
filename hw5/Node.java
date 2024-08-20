6155d4dc95a645711e9fc98821b2ab51e72a1f69
TeetatB <teetatmix36108@gmail.com>
2024-08-20 15:55:22 +0700

    commenting Node and Queue

--- hw5/Node.java
+++ hw5/Node.java
@@ -1,51 +1,51 @@
-public class Node extends BTreePrinter { // Fix this line
+public class Node extends BTreePrinter { //setting Node to inherit the traits of BTreePrinter which is a class
 
-    Node left;
+    Node left; 
     Node right;
     int data;
 
-    public Node(int data) {
+    public Node(int data) { //create constructor to contain an input data when create a new Node 
         // Add something
         this.data = data;
-        this.left = null;
+        this.left = null; 
         this.right = null;
     }
 
-    public void printTree() {
-        super.printTree(this);
+    public void printTree() { //function that print Tree structure
+        super.printTree(this);  //use function printTree from class BTreePrinter 
     }
 
-    public void printBFT() {
-        Queue q = new Queue(50);
+    public void printBFT() { //function that print data of each node in Tree by Breadth first traversal method 
+        Queue q = new Queue(50); //create a queue to implement Tree by BFT method
         System.out.print("BFT node sequence [ ");
         // Do something
-        q.enqueue(this);
-        while (!q.isEmpty()) {
-            Node curr = q.dequeue();
-            System.out.print(curr.data + " ");
-            if (curr.left != null) {
-                q.enqueue(curr.left);
+        q.enqueue(this); //enqueue root node to queue
+        while (!q.isEmpty()) { //loop that continue until queue is empty
+            Node curr = q.dequeue(); //create node name curr to contain first node of queue after dequeue root node from queue
+            System.out.print(curr.data + " "); //print curr node's data
+            if (curr.left != null) { //check if the first node in queue has left node
+                q.enqueue(curr.left); //if the node has left node ,then enqueue that node's left node into queue
             }
-            if (curr.right != null) {
-                q.enqueue(curr.right);
+            if (curr.right != null) { //check if the first node in queue has right node
+                q.enqueue(curr.right); //if the node has right node, then enqueue that node's right node into queue
             }
         }
         System.out.println("]");
     }
 
-    public void printDFT() { // PreOrder
-        Stack s = new Stack(50);
+    public void printDFT() { // function that print data of each node in Tree by Depth first traversal method (Preorder)
+        Stack s = new Stack(50); //create a stack to implement Tree by DFT method (Preorder)
         System.out.print("DFT node sequence [ ");
         // Do something
-        s.push(this);
-        while (!s.isEmpty()) {
-            Node curr = s.pop();
-            System.out.print(curr.data + " ");
-            if (curr.right != null) {
-                s.push(curr.right);
+        s.push(this); //push root node into stack
+        while (!s.isEmpty()) { //loop that continue until stack is empty
+            Node curr = s.pop(); //create node name curr to contain root node after pop root node from stack
+            System.out.print(curr.data + " "); //print curr node's data
+            if (curr.right != null) { //check if the last node in stack has right node
+                s.push(curr.right); // if the node has right node, then push that node's right node into stack 
             }
-            if (curr.left != null) {
-                s.push(curr.left);
+            if (curr.left != null) { //check if the last node in stack has left node
+                s.push(curr.left); //if the node has left node, then push that node's left node into stack
             }
         }
         System.out.println("]");
--- hw5/Queue.java
+++ hw5/Queue.java
@@ -1,73 +1,73 @@
-public class Queue {
+public class Queue { //create a class queue that implement circular array
     Node[] arr; // circular array
     int capacity;
     int front;
     int back;
     int size;
     
-    public Queue(int cap){
-        this.arr = new Node[cap];
+    public Queue(int cap){ //create constructor that create array that has capacity as input 
+        this.arr = new Node[cap]; 
         this.capacity = cap;
         this.front = 0;
         this.back = 0;
         this.size = 0;
     }
     
-    public void enqueue(Node node){
-        if (!isFull()){
-            // do something
-            arr[back++] = node;
-            if (back == capacity) {
-                back = 0;
+    public void enqueue(Node node){ //function that enqueue a node into queue
+        if (!isFull()){ //check if queue is not empty
+            //when queue is not full
+            arr[back++] = node; //set index back+1 of array to contain node   
+            if (back == capacity) { //check if back is at the last index of array
+                back = 0; //set back into index 0 of array
             }
-            size++;
+            size++; //increase size 
             
-        }else{
+        }else{ //if queue is empty print Queue Overflow
             System.out.println("Queue Overflow!!!");
         }
     }
     
-    public Node dequeue(){
+    public Node dequeue(){ //function that dequeue a node from queue
         
-        if (!isEmpty()){
+        if (!isEmpty()){ //check if queue is not empty
             // do something
-            Node tmp = arr[front++];
-            if (front == capacity) {
-                front = 0;
+            Node tmp = arr[front++]; //create a node name tmp to contain node at index front+1 of array 
+            if (front == capacity) { //check if front is at the last index of array 
+                front = 0; //set front into index 0 of array
             }
-            size--;
-            return tmp;
+            size--; //decrease size
+            return tmp; //return tmp 
             
-        }else{
+        }else{ //if queue is empty print Queue Underflow
             System.out.println("Queue Underflow!!!");
         }
         return null; // fix this (out of place)
     }
     
-    public boolean isEmpty(){
+    public boolean isEmpty(){ //function that return if size is 0
         return size == 0; // fix this
     }
     
-    public boolean isFull(){
+    public boolean isFull(){ //function that return if size equal capacity
         return size == capacity; // fix this
     }
     
-    public void printCircularIndices(){
+    public void printCircularIndices(){ //function that print front and back index of array
         System.out.println("Front index = " + front + " Back index = " + back);
     }
     
-    public void printQueue(){
-        if (!isEmpty()){
-            System.out.print("[Front] ");
-            int curr = front;
-            for (int i = 0; i < size; i++) {
+    public void printQueue(){ //function that print queue
+        if (!isEmpty()){ //check if queue is not empty
+            System.out.print("[Front] "); 
+            int curr = front; //create integer name curr to contain value of front
+            for (int i = 0; i < size; i++) { //loop that continue until i equal size of member in array 
                 System.out.print(arr[curr++].data + " ");
-                if (curr == capacity) {
-                    curr = 0;
+                if (curr == capacity) { //check if curr equal capacity
+                    curr = 0; //if curr is the last index of array set it to the first index of array 
                 }
             }
             System.out.println("[Back]");
-        }else{
+        }else{ //if queue is empty print Empty Queue
             System.out.println("Empty Queue!!!");
         }
     }
