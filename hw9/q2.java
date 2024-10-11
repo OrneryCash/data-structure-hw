05b4d1c8696d3212b7a61fbb3ff7c4d04836c5da
TeetatB <teetatmix36108@gmail.com>
2024-10-11 19:24:19 +0700

    comment q2-3

--- hw9/q2.java
+++ hw9/q2.java
@@ -7,18 +7,19 @@ public class HashTable {
     int p; // Big Prime (for PolyHash())
     int x; // Small number (for PolyHash())
     
-    public HashTable(int cap, int p, int x){
+    public HashTable(int cap, int p, int x){ 
+        // create an string array with cap size 
         arr = new String[cap];
-        this.capacity = cap;
-        this.p = p;
-        this.x = x;
+        this.capacity = cap; //set capacity of this class to be cap value
+        this.p = p; //set p of this class to be p value
+        this.x = x; //set x of this class to be x value
     }
     
     public void addString(String s){
         // Insert the string at that index that no one occupy
         // You should call getIndex()
-        int index = getIndex(s);
-        arr[index] = s;
+        int index = getIndex(s); //initialize index value by index value of getIndex function
+        arr[index] = s; //change value of array index index to s value 
     }
     
     // This function has 2 roles
@@ -30,11 +31,11 @@ public class HashTable {
     // 3. If the arr[index] equals the string, then also return index
     // 4. Perform the quadratic probing and repeat 2.-3. until found the location
     public int getIndex(String s){
-        int index = polyHash(s, p, x);
+        int index = polyHash(s, p, x); //initialize index value as hash value from polyHash function 
         
-        for (int i = 0; i < capacity; i++) {
-            index = (index + i) % capacity;
-            if (arr[index] == null || arr[index] == s) return index;
+        for (int i = 0; i < capacity; i++) { //create a loop that change index value 
+            index = (index + i) % capacity; //change indeex value to be (index plus i ) mod capacity 
+            if (arr[index] == null || arr[index] == s) return index; //if index of array is null or is same as string s return index
         }
         return 0; // Fix this
     }
@@ -42,21 +43,21 @@ public class HashTable {
     // Check the slide for the pseudocode
     // If you want to access a char of string s at index i, use s.charAt(i)
     // If you want to get the ASCII code of char c, use (int)c
-    public static int polyHash(String s, int p, int x){
-        int hash = 0;
+    public static int polyHash(String s, int p, int x){ //function that hash string 
+        int hash = 0; //initialize hash value to be 0 
         // Do something
-        for (int i = s.length()-1; i >= 0; i--) {
-            hash = (hash * x + (int) s.charAt(i)) % p;
+        for (int i = s.length()-1; i >= 0; i--) { //create loop that change hash value until i equal s length - 1
+            hash = (hash * x + (int) s.charAt(i)) % p; //change hash value to be as the hash formula 
         }
-        return hash;
+        return hash; 
     }
     
     // This is editable testcase, no need to edit
     public static void test1(){
         int p = 1100101;    // Big Prime    (Hash key1)
         int x = 1001;       // Small number (Hash key2)
-        HashTable table = new HashTable(16, p, x);
-        String[] names = {"a", "b", "c", "A", "B", "BA", "BBA", "aaa", "aaaaa", "0","1","11","111", "abcdABCD01234" };
+        HashTable table = new HashTable(16, p, x);  
+        String[] names = {"a", "b", "c", "A", "B", "BA", "BBA", "aaa", "aaaaa", "0","1","11","111", "abcdABCD01234" }; 
         for (int i=0; i<names.length; i++)
             System.out.println("HashCode of '" + names[i] + "' is "+HashTable.polyHash(names[i], p, x));
     }
@@ -65,7 +66,7 @@ public class HashTable {
     public static void test2(){
         int p = 1100101;    // Big Prime    (Hash key1)
         int x = 1001;       // Small number (Hash key2)
-        HashTable table = new HashTable(16, p, x);
+        HashTable table = new HashTable(16, p, x); 
         String[] names = {"a", "b", "c", "A", "B", "BA", "BBA", "aaa", "aaaaa", "0","1","11","111", "abcdABCD01234" };
 
         for (int i=0; i<names.length; i++){
--- hw9/q3.java
+++ hw9/q3.java
@@ -12,17 +12,18 @@ public class Graph {
     int size;
     int cc;
 
-    public Graph(int cap){
+    public Graph(int cap){ // Initialize constructor for graph
         // Pls initialize all varibles here
-        this.vertexList = new Vertex[cap];
-        this.adjacencyList = new LinkedList[cap];
-        this.cap = cap;
-        this.size = 0;
-        this.cc = 0;
+        this.vertexList = new Vertex[cap]; //create array that contain vertices and has cap size
+        this.adjacencyList = new LinkedList[cap]; //create array that contain adjacency vertices and has cap size 
+        this.cap = cap; //Initialize cap 
+        this.size = 0; //Initialize size = 0
+        this.cc = 0; //initialize cc
     }
     
     public void addVertex(int key){
-        if (size==cap){
+         //create function to check if vertex list is full
+        if (size==cap){ //if size equal cap print vertex list is full and return nothing
             System.out.println("Vertex list is full. You need to recreate the Graph");
             return;
         }
@@ -30,67 +31,71 @@ public class Graph {
         // Create Vertex object and the LinkedList object
         // Add these objects to the corresponding arrays
         // finally, size++;
-        Vertex v = new Vertex(key);
-        vertexList[key] = v;
-        LinkedList<Integer> l = new LinkedList<>();
-        adjacencyList[key] = l;
-        size++;
+
+        //if vertex list is not full 
+        Vertex v = new Vertex(key); //create new vertex 
+        vertexList[key] = v; //add vertex into vertex list
+        LinkedList<Integer> l = new LinkedList<>(); //create new edge 
+        adjacencyList[key] = l; //add edge to array of adjacencyList
+        size++; //after adding vertex increase the size of vertex list
     }
     
     // Two way edge
     // If you make a path from u to v, you must make a path from v to u
-    public void addEdge(int u, int v){
-        if (vertexList[u]==null){
+    public void addEdge(int u, int v){ //create edge from vertex to vertex
+        //create function to check if vertexList is null
+        if (vertexList[u]==null){ //if source vertexList is null print source node does not exist and return nothing
             System.out.println("Source node does not exist");
             return;
-        } if (vertexList[v]==null){
+        } if (vertexList[v]==null){ //if destination vertex is null print Destination node does not exist and return nothing
             System.out.println("Destination node does not exist");
             return;
         }
         
         // Check if there is already an edge from u to v
-        if (!isConnected(u, v)){
+        if (!isConnected(u, v)){ //if both vertex is not connected to each other
             // Add edge from Vertex(u) to Vertex(v)
             adjacencyList[u].add(v);
             // and then from Vertex(v) to Vertex(u)
             adjacencyList[v].add(u);
             
-        }else{
+        }else{ //if vertex is already connected to each other, then print There is already an edge connecting vertex u and vertex v
             System.out.println("There is already an edge connecting vertex "+u+" and vertex "+v);
         }
     }
     
     // Check if Vertex(u) and Vertex(v) has an edge to one another
     public boolean isConnected(int u, int v){
-        return adjacencyList[u].contains(v); // Fix this
+        return adjacencyList[u].contains(v); // return boolean that check if u has path to v
     }
 
     // This function is complete, no need to edit
     // Please study how this function works
     // if you understand this function, you will undertand the whole homework
     public void showAdjacentVertices(int u){
-        Vertex v = vertexList[u];
-        System.out.print("Vertex " + v.strKey + " connected to the following vertices: ");
+        Vertex v = vertexList[u]; //create vertex v that initailize as vertexList index u 
+        System.out.print("Vertex " + v.strKey + " connected to the following vertices: "); //print vertex v's strkey 
         
-        LinkedList<Integer> list = adjacencyList[u];
-        for (int vertex_index : list) {
+        LinkedList<Integer> list = adjacencyList[u]; //create list that contains adjacencyList index u  
+        for (int vertex_index : list) { //loop that print each strkey of vertex in vertexlist
             System.out.print(vertexList[vertex_index].strKey + ", ");
         }
         System.out.println();
     }  
     
-    public void BFS(Vertex s){
+    public void BFS(Vertex s){ 
       // Set all Vertex.dist to Infinity (Use Integer.MAX_VALUE to represent Infinity)
-      for (int i = 0; i < cap; i++) {
-          if (vertexList[i] != null) {
+      for (int i = 0; i < cap; i++) { //loop that run until i equals cap
+          //check if vertexList is null
+          if (vertexList[i] != null) { //if vertexList at index i is not null set distance of that vertex to infinity
             vertexList[i].dist = Integer.MAX_VALUE;
           }
       }
       // Set dist of the start vertex (s) to 0
       // Push the start vertex to an empty queue
-      s.dist = 0;
-      Queue<Vertex> q = new LinkedList<>();
-      q.add(s);
+      s.dist = 0; 
+      Queue<Vertex> q = new LinkedList<>(); //create queue
+      q.add(s); //add vertex s to q
       // [*] Check if the queue is not empty
       // Pop the queue, and get the current vertex
       // Extract the list of all vertices that are connected to current vertex
@@ -101,35 +106,37 @@ public class Graph {
       //          set the prev variable of that vertex to the current vertex
       
       // Repeat [*]
-      while (!q.isEmpty()) {
-          Vertex curr = q.remove();
-          LinkedList<Integer> adj = adjacencyList[curr.intKey];
-          for (int i = 0; i < adj.size(); i++) {
-              Vertex vertex = vertexList[adj.get(i)];
-              if (vertex.dist == Integer.MAX_VALUE) {
-                  q.add(vertex);
-                  vertex.dist = curr.dist + 1;
-                  vertex.prev = curr;
+      while (!q.isEmpty()) { //loop that run while q of vertex is not empty
+          Vertex curr = q.remove(); //dequeue vertex and set curr to contain the value we dequeue
+          LinkedList<Integer> adj = adjacencyList[curr.intKey]; //create linkedlist that contains adjacencyList at index key of vertex curr
+          for (int i = 0; i < adj.size(); i++) { //loop that continue until i equals adj size 
+              Vertex vertex = vertexList[adj.get(i)]; //create vertex that value is vertex at index i of vertexlist
+              //check if vertex distance isequal infinity
+              if (vertex.dist == Integer.MAX_VALUE) { //if condition is true
+                  q.add(vertex); //add vertex to queue
+                  vertex.dist = curr.dist + 1; //set vertex distance to be curr distance + 1
+                  vertex.prev = curr; //set previous vertex to curr
               }
           }
       }
   }
 
   
-  public Stack getShortestPathList(Vertex S, Vertex U){
+  public Stack getShortestPathList(Vertex S, Vertex U){ 
     //FIX THIS
-    if (U.prev == null) {
+    //check if vertex U 's previous vertex is null 
+    if (U.prev == null) { //if previous vertex of U is null, then print There is no path from s.intkey to U.intkey and return empty stack
       System.out.printf("There is no path from %d to %d", S.intKey, U.intKey);
-      return new Stack();
+      return new Stack(); 
     }
       // Create a stack
-      Stack<Vertex> s = new Stack<>();
+      Stack<Vertex> s = new Stack<>(); 
       // Start from Vertex U
       // [*] push the current Vertex into the stack
       // Go back one Vertex using U.prev
       // Repeat [*] until you reach the Vertex S
-      Vertex curr = U;
-      while (curr != S) {
+      Vertex curr = U; //create vertex curr and initialize its value to be as U
+      while (curr != S) { //push curr to stack and curr's previous vertex until curr is not equal S
           s.push(curr);
           curr = curr.prev;
       }
@@ -140,18 +147,18 @@ public class Graph {
   
   public void printShortestPath(int s_index, int u_index){
     
-      // Map Vertex names to index numbers
-      // Get vertices from the vertexList
-      // Run BFS() at the starting Vertex
+      
+      
+      
       // Get shortestPartList(starting Vertex, ending Vertex)
       // Traverse all the stack and print the Vertex name
-      BFS(vertexList[s_index]);
-      Stack<Vertex> s = getShortestPathList(vertexList[s_index], vertexList[u_index]);
-      if (s.empty()) return;
-      while (s.peek() != vertexList[u_index]) {
+      BFS(vertexList[s_index]); // Map Vertex names to index numbers
+      Stack<Vertex> s = getShortestPathList(vertexList[s_index], vertexList[u_index]); // Get vertices from the vertexList
+      if (s.empty()) return; //if stack is empty return nothing
+      while (s.peek() != vertexList[u_index]) {  //loop that print key of vertex that pop out of stack while first vertex of stack is not equal vertex at index u
           System.out.print(s.pop().intKey + " -> ");
       }
-      System.out.println(s.pop().intKey);
+      System.out.println(s.pop().intKey);  //print last vertex
   }
     
     
